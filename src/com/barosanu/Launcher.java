7a50e2ff42987b5a1c5c273db7afabcdd4b0c29b
Lukasz Kublik <lukasz.kublik.programista@gmail.com>
2024-06-21 07:11:21 +0200

    Added classes to comunicate between controllers and views

--- src/com/barosanu/Launcher.java
+++ src/com/barosanu/Launcher.java
@@ -1,9 +1,12 @@
 package com.barosanu;
 
+import com.barosanu.view.ViewFactory;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
@@ -15,12 +18,8 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent parent = FXMLLoader.load(getClass().getResource("view/MainWindow.fxml"));
-
-        Scene scene = new Scene(parent);
-        stage.setScene(scene);
-
-        stage.show();
+        ViewFactory viewFactory = new ViewFactory(new EmailManager());
+        viewFactory.showLoginWindow();
 
     }
 }
--- src/com/barosanu/controller/LoginWindowController.java
+++ src/com/barosanu/controller/LoginWindowController.java
@@ -1,24 +1,29 @@
 package com.barosanu.controller;
 
+import com.barosanu.EmailManager;
+import com.barosanu.view.ViewFactory;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-public class LoginWindowController {
+public class LoginWindowController extends BaseController {
 
     @FXML
-    private TextField emailAddressField;
+    private Button errorLabel;
 
     @FXML
-    private Label errorLabel;
+    private TextField emailAddressFied;
 
     @FXML
     private PasswordField passwordField;
 
+    public LoginWindowController(EmailManager emailManager, ViewFactory viewFactory, String fxmlName) {
+        super(emailManager, viewFactory, fxmlName);
+    }
+
     @FXML
     void loginButtonAction() {
-        System.out.println("Click!!");
-
+        System.out.println("cliK!!");
     }
 }
