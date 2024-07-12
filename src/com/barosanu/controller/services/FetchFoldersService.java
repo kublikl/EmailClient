2fce733644d12f0edfa66579a64fd4c9b667a7a6
Lukasz Kublik <lukasz.kublik.programista@gmail.com>
2024-07-12 06:38:54 +0200

    Added folder icons

--- src/com/barosanu/controller/services/FetchFoldersService.java
+++ src/com/barosanu/controller/services/FetchFoldersService.java
@@ -2,6 +2,7 @@
 package com.barosanu.controller.services;
 
 import com.barosanu.model.EmailTreeItem;
+import com.barosanu.view.IconResolver;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
@@ -18,6 +19,7 @@ public class FetchFoldersService extends Service<Void> {
     private Store store;
     private EmailTreeItem<String> foldersRoot;
     private List<Folder> folderList;
+    private IconResolver iconResolver = new IconResolver();
 
     public FetchFoldersService(Store store, EmailTreeItem<String> foldersRoot, List<Folder> folderList) {
         this.store = store;
@@ -45,6 +47,7 @@ public class FetchFoldersService extends Service<Void> {
         for(Folder folder: folders){
             folderList.add(folder);
             EmailTreeItem<String> emailTreeItem = new EmailTreeItem<String>(folder.getName());
+            emailTreeItem.setGraphic(iconResolver.getIconForFolder(folder.getName()));
             foldersRoot.getChildren().add((emailTreeItem));
             foldersRoot.setExpanded(true);
             fetchMessagesOnFolder(folder, emailTreeItem);
